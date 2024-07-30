dd29a4bda499ddae9cc2ead756e473e0e518c60a
Florin12er <apetreiflorin236@gmail.com>
2024-07-30 23:37:47 +0200

    fixed the styles and adjusted the modal provider location

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { Toaster } from "sonner";
 import { EdgeStoreProvider } from "@/lib/edgestore";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -45,6 +46,7 @@ export default function RootLayout({
               storageKey="jotion-theme"
             >
               <Toaster position="bottom-center" />
+              <ModalProvider />
               {children}
             </ThemeProvider>
           </EdgeStoreProvider>
